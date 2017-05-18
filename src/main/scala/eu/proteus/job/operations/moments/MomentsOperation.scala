8666d43fec98b726a277fd22cef46f5a212ab549
Ventura Del Monte <venturadelmonte@gmail.com>
2017-05-18 15:55:47 +0200

    Change simple moments output format to include x[,y] coil coords

--- src/main/scala/eu/proteus/job/operations/moments/MomentsOperation.scala
+++ src/main/scala/eu/proteus/job/operations/moments/MomentsOperation.scala
@@ -16,49 +16,115 @@
 
 package eu.proteus.job.operations.moments
 
-import eu.proteus.job.operations.data.model.CoilMeasurement
-import eu.proteus.job.operations.data.results.MomentsResult
+import java.beans.Transient
+
+import eu.proteus.job.operations.data.model.{CoilMeasurement, SensorMeasurement1D, SensorMeasurement2D}
+import eu.proteus.job.operations.data.results.{MomentsResult, MomentsResult1D, MomentsResult2D}
 import eu.proteus.solma.moments.MomentsEstimator
+import org.apache.flink.api.common.state.{MapState, MapStateDescriptor}
 import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.functions.co.RichCoFlatMapFunction
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.util.Collector
+
+import scala.collection.mutable
 
 object MomentsOperation {
 
   def runSimpleMomentsAnalytics(
       stream: DataStream[CoilMeasurement],
       featuresCount: Int
- ): DataStream[MomentsResult] = {
+  ): DataStream[MomentsResult] = {
 
     implicit val typeInfo = TypeInformation.of(classOf[(CoilMeasurement, Int)])
+
+    var kstream: KeyedStream[(CoilMeasurement, Int), Int] = null
 
     val momentsEstimator = MomentsEstimator()
       .setFeaturesCount(1)
       .enableAggregation(false)
       .setPartitioning((in) => {
-        in
+
+        kstream = in
           .asInstanceOf[DataStream[(CoilMeasurement)]]
           .map(
-          (coilMeasurement: CoilMeasurement) => {
-            val sensorId = coilMeasurement.slice.head
-            coilMeasurement.slice = 0 to 0
-            (coilMeasurement, coilMeasurement.coilId * featuresCount + sensorId)
+            (coilMeasurement: CoilMeasurement) => {
+              val sensorId = coilMeasurement.slice.head
+              (coilMeasurement, coilMeasurement.coilId * featuresCount + sensorId)
             }
           )
           .keyBy(x => x._2)
-          .asInstanceOf[KeyedStream[(Any, Int), Int]]
+
+        kstream.map(
+          (t) => {
+            val (coilMeasurement, pid) = t
+            val clone = coilMeasurement match {
+              case s1d: SensorMeasurement1D => s1d.copy(slice = 0 to 0)
+              case s2d: SensorMeasurement2D => s2d.copy(slice = 0 to 0)
+            }
+            (clone.asInstanceOf[CoilMeasurement], pid)
+            }
+        )
+        .keyBy(x => x._2)
+        .asInstanceOf[KeyedStream[(Any, Int), Int]]
       })
 
     val moments = momentsEstimator.transform(stream)
 
-    moments.flatMap((t, out) => {
-      val (pid: Int, metrics: MomentsEstimator.Moments) = t
-      val variance = metrics.variance(0)
-      if (!java.lang.Double.isNaN(variance)) {
-        val cid = pid % featuresCount
-        val sid = pid / featuresCount
-        out.collect(MomentsResult(cid, sid, metrics.mean(0), variance, metrics.counter(0)))
-      }
+    kstream
+      .connect(moments.keyBy(x => x._1))
+      .flatMap(new RichCoFlatMapFunction[(CoilMeasurement, Int), (Int, MomentsEstimator.Moments), MomentsResult]() {
+
+        @Transient
+        private var map: MapState[Int, mutable.Queue[(Option[Double], Option[Double])]] = _
+
+        override def open(parameters: Configuration): Unit = {
+          super.open(parameters)
+          val descriptor = new MapStateDescriptor[Int, mutable.Queue[(Option[Double], Option[Double])]](
+            "coords",
+            TypeInformation.of(classOf[Int]),
+            TypeInformation.of(classOf[mutable.Queue[(Option[Double], Option[Double])]])
+          )
+          map = getRuntimeContext.getMapState(descriptor)
+        }
+
+        override def flatMap1(value: (CoilMeasurement, Int), out: Collector[MomentsResult]): Unit = {
+          val key = value._1.coilId * featuresCount + value._1.slice.head
+          val v = value._1 match {
+            case s1d: SensorMeasurement1D => (Some(s1d.x), None)
+            case s2d: SensorMeasurement2D => (Some(s2d.x), Some(s2d.y))
+          }
+          val queue = if (map.contains(key)) {
+            map.get(key)
+          } else {
+            val q = mutable.Queue[(Option[Double], Option[Double])]()
+            map.put(key, q)
+            q
+          }
+          queue += v
+        }
+
+        override def flatMap2(in: (Int, MomentsEstimator.Moments), out: Collector[MomentsResult]): Unit = {
+          val (pid: Int, metrics: MomentsEstimator.Moments) = in
+          val cid = pid % featuresCount
+          val sid = pid / featuresCount
+          val q = map.get(pid)
+          if (q.isEmpty) {
+            throw new RuntimeException("this queue is not supposed to be empty!")
+          }
+          val (ox, oy) = q.dequeue
+          val result = oy match {
+            case Some(y) => MomentsResult2D(cid, sid, metrics.mean(0), ox.get, y, metrics.variance(0), metrics.counter(0))
+            case None => MomentsResult1D(cid, sid, metrics.mean(0), ox.get, metrics.variance(0), metrics.counter(0))
+          }
+          out.collect(result)
+        }
+
     })
+    .name("coil-xy-join")
+    .uid("coil-xy-join")
+
 
   }
 
